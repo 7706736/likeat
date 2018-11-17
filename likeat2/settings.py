4ef7dc6d52dba47817ad59cb361b3318ae63eb3b
7706736 <arnulfocaviedes@gmail.com>
2018-11-17 15:01:13 -0500

    First version of application moved into github

--- likeat2/settings.py
+++ likeat2/settings.py
@@ -22,10 +22,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ys3q8sv-a1er*6s)na1no^bp7bwttx15&4et=19)=tcajlzc8x'
+# SECRET_KEY = 'ys3q8sv-a1er*6s)na1no^bp7bwttx15&4et=19)=tcajlzc8x'
+
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
 ALLOWED_HOSTS = []
 
@@ -45,6 +49,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',   # agregado para desplegar el proyecto en internet
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -142,3 +147,16 @@ EMAIL_PORT = 25
 EMAIL_HOST_USER = 'arnulfocaviedes@gmail.com'
 EMAIL_HOST_PASSWORD = 'Jehova.Dios'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+# The absolute path to the directory where collectstatic will collect static files for deployment.
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# lo anterior es para reducir el tamaño de los archivos estáticos, haciendo mas eficiente la aplicación

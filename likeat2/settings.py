27b0f12cb5a7e4ea786c4a95dc1df60a2a2b6d72
7706736 <arnulfocaviedes@gmail.com>
2018-11-17 15:34:01 -0500

    actualizado ALLOWED_HOSTS con el sitio y la URL del servidor de desarrollo

--- likeat2/settings.py
+++ likeat2/settings.py
@@ -24,14 +24,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = 'ys3q8sv-a1er*6s)na1no^bp7bwttx15&4et=19)=tcajlzc8x'
 
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'ys3q8sv-a1er*6s)na1no^bp7bwttx15&4et=19)=tcajlzc8x')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['likeat.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
